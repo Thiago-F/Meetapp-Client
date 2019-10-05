f5c69cdffad7d2de670d316ccd6c60956d8d26a4
Thiago-F <thiago_fialho150@hotmail.com>
2019-10-05 11:30:02 -0300

    add unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import { ReactComponent as LogoM } from '~/assets/M.svg';
 
 export default function SignIn() {
+    function handleSubmit(data) {
+        console.log(data);
+    }
+
     return (
         <>
             <LogoM alt="Meetapp" />
-            <form>
-                <input type="email" placeholder="Digite seu e-mail" />
-                <input type="password" placeholder="Sua senha secreta" />
-                <button type="button" className="btn">
+            <Form onSubmit={handleSubmit}>
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="Digite seu e-mail"
+                />
+                <Input
+                    name="password"
+                    type="password"
+                    placeholder="Sua senha secreta"
+                />
+                <button type="submit" className="btn">
                     Entrar
                 </button>
                 <Link to="/register">Criar conta grátis</Link>
-            </form>
+            </Form>
         </>
     );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import { ReactComponent as LogoM } from '~/assets/M.svg';
 
 export default function SignUp() {
+    function handleSubmit(data) {
+        console.log(data);
+    }
+
     return (
         <>
             <LogoM alt="Meetapp" />
-            <form>
-                <input type="text" placeholder="Nome Completo" />
-                <input type="email" placeholder="Digite seu e-mail" />
-                <input type="password" placeholder="Sua senha secreta" />
-                <button type="button" className="btn">
+            <Form onSubmit={handleSubmit}>
+                <Input name="name" type="text" placeholder="Nome Completo" />
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="Digite seu e-mail"
+                />
+                <Input
+                    name="password"
+                    type="password"
+                    placeholder="Sua senha secreta"
+                />
+                <button type="submit" className="btn">
                     Criar conta
                 </button>
                 <Link to="/">Já tenho login</Link>
-            </form>
+            </Form>
         </>
     );
 }
