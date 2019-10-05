bad461ae625ea2eba449a380554d32fdf42efdac
Thiago-F <thiago_fialho150@hotmail.com>
2019-10-05 09:21:03 -0300

    add router

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {BrowserRouter , Route , Switch} from 'react-router-dom';
+
+import Home from './pages/Home'
+
+
+export default function router() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
