cc5cd47de3b053415b8136a1f6adbf4bdeaab04f
Thiago-F <thiago_fialho150@hotmail.com>
2019-10-05 09:37:32 -0300

    add editorconfig prettier eslint

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import {BrowserRouter , Route , Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Home from './pages/Home'
-
+import Home from './pages/Home';
 
 export default function router() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Home}/>
+                <Route path="/" exact component={Home} />
             </Switch>
         </BrowserRouter>
-    )
+    );
 }
