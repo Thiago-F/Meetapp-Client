adf615aa0eccfe71b4120e0e1167390a53ab2161
Thiago-F <thiago_fialho150@hotmail.com>
2019-10-10 18:24:34 -0300

    add yup

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -5,15 +5,24 @@ import { Form, Input } from '@rocketseat/unform';
 import BannerInput from './BannerInput';
 import { Container } from './styles';
 
-const schema = Yup.object().shape({});
+import {useSelector} from 'react-redux'
+
+const schema = Yup.object().shape({
+    title : Yup.string().required('O Título é obrigatório'),
+    desc: Yup.string().required('A descrição é obrigatório').min(3),
+    date : Yup.date().required('A data é obrigatório'),
+    locale : Yup.string().required('A localização é obrigatório')
+});
 
 export default function New() {
     function handleSubmit(data) {}
     function handleProcess(data) {}
 
+    const meetup = useSelector(state => state.meetup)
+
     return (
         <Container>
-            <Form onSubmit={handleSubmit} schema={schema}>
+            <Form onSubmit={handleSubmit} schema={schema} initialData>
                 <BannerInput name="banner_id" />
                 <Input name="title" placeholder="Título do Meetup" />
                 <Input name="desc" multiline placeholder="Descrição completa" />
